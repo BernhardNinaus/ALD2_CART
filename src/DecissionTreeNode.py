f0dfa47e1cee28f375d56659e3c384115616946d
Bernhard Ninaus <ninausbernhard@gmail.com>
2021-06-21 14:20:25 +0200

    DecisionTreeClassifier implementiert. Sample (titanic.py) hinzugefügt und output erstellt.

---
+++ src/DecissionTreeNode.py
@@ -0,0 +1,62 @@
+from typing import *
+
+class DecissionTreeNode:
+    __slots__ = ["left", "right", "chance", "feature", "combination"]
+
+    def __init__(self):
+        self.left: DecissionTreeNode = None 
+        self.right: DecissionTreeNode = None
+        self.chance: float = None
+
+        self.feature: str = None
+        self.combination: Any = None
+  
+    def _display_aux(self):
+        '''
+        Returns list of strings, width, height, and horizontal coordinate of the root.\r\n
+        https://stackoverflow.com/a/54074933/12452156
+        '''
+        if self.right is None and self.left is None:
+            line = f"{round(self.chance * 100,1)}%"
+            width = len(line)
+            return [line], width, 1, width // 2
+
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self._node_text()
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self._node_text()
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        left, n, p, x = self.left._display_aux() if self.left != None else self._left_text()
+        right, m, q, y = self.right._display_aux() if self.right != None else self._right_text()
+        s = '%s' % self._node_text()
+
+        u = len(s)
+        first_line = (x + 1) * ' '  + (n - x - 1) * '_'         + s  + y * '_'        + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' '             + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    def _node_text(self):
+        return f"_<- {self.feature} {self.combination}|surv:{round(self.chance * 100,1)}%"
+
+    def __str__(self):
+        lines, *_ = self._display_aux()
+        return "\r".join(lines)
---
+++ src/titanic.py
@@ -0,0 +1,17 @@
+from DecisionTreeClassifier import *
+import pandas as pd
+
+titanic = pd.read_csv("data/titanic.csv")
+
+target = titanic["Survived"]
+features = titanic.loc[:, ["Pclass", "Sex", "Age"]] 
+
+# Pclass wird von numerischen zu muliclass geändert um gini richtig zu berechnen
+features.Pclass = features.Pclass.map({1: "1st", 2: "2nd", 3: "3rd"})
+
+tree_builder = DecisionTreeClassifier(target, features, max_depht=3)
+tree_builder.buildDT()
+
+f = open("output.txt", "w")
+f.write(str(tree_builder))
+f.close()
